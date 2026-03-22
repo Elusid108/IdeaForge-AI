0d237033cd3af5d8f7c0a20c9c54e3550e2e89bb
Chris Moore <103774436+Elusid108@users.noreply.github.com>
2026-03-22 17:10:19 -0400

    Deploy?

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/IdeaForge-AI/",
   server: {
     host: "::",
     port: 8080,
